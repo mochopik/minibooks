b2ec705caf2fff08e19933406bdeb443027d9a0e
Taufik Hidayat <taufikhdt@outlook.com>
2018-03-06 13:16:27 +0700

    Setting up google oauth2.0

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+
+// Passport config
+require('./config/passport')(passport);
+
+// Load Router
+const auth = require('./routes/auth');
 
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Its Works!')
+    res.send('Its Works!');
 });
+
+// Use Routes
+app.use('/auth', auth);
 
 const port  = process.env.PORT || 5000;
 
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb://mochopik:admin@ds155587.mlab.com:55587/db_minibook-dev',
+    googleClientID: '1070242276214-c5juom1248jgp6fucgbh9to676i8g610.apps.googleusercontent.com',
+    googleClientSecret: 'NgOjYbcrLIfbSYio8RWh1-QF'
+}
