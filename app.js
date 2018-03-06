928064ae82f6481992eeea1a11fe014284c41d6e
Taufik Hidayat <taufikhdt@outlook.com>
2018-03-06 11:55:57 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Its Works!')
+});
+
+const port  = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
