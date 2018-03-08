67b788a2ab6e412a4f84f64a19ca9bea06324a9f
Taufik Hidayat <taufikhdt@outlook.com>
2018-03-08 16:57:48 +0700

    Adding config key for productuon

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-    mongoURI: 'mongodb://mochopik:admin@ds155587.mlab.com:55587/db_minibook-dev',
-    googleClientID: '1070242276214-c5juom1248jgp6fucgbh9to676i8g610.apps.googleusercontent.com',
-    googleClientSecret: 'NgOjYbcrLIfbSYio8RWh1-QF'
+if(process.env.NODE_ENV === 'production') {
+    module.exports = require ('./keys_prod');
+} else {
+    module.exports = require ('./keys_dev');
 }
