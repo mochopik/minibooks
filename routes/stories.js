f9568c87e81a9cb797f55735ec831c8c95f09a7c
Taufik Hidayat <taufikhdt@outlook.com>
2018-03-08 16:31:39 +0700

    Show stories for spescific users

--- routes/stories.js
+++ routes/stories.js
@@ -18,6 +18,28 @@ router.get('/', (req, res) => {
         });
 });
 
+// List Stories from a user
+router.get('/user/:userId', (req, res) => {
+    Story.find({user: req.params.userId, status: 'public'})
+        .populate('user')
+        .then(stories => {
+            res.render('stories/index', {
+                stories: stories
+            });
+        });
+});
+
+// Login user stories
+router.get('/my', ensureAuthenticated, (req, res) => {
+    Story.find({user: req.user.id})
+        .populate('user')
+        .then(stories => {
+            res.render('stories/index', {
+                stories: stories
+            });
+        });
+});
+
 // Show single story
 router.get('/show/:id', (req, res) => {
     Story.findOne({
